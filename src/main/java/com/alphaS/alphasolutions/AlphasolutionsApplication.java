099ddfee30b9a8c34254c5a7b4f792928bfbc78f
najamoe <naja0718@stud.kea.dk>
2023-04-26 16:23:13 +0200

    Start commit to link the project with git

---
+++ src/main/java/com/alphaS/alphasolutions/AlphasolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.alphaS.alphasolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphasolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphasolutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alphaS/alphasolutions/AlphasolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphaS.alphasolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphasolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
