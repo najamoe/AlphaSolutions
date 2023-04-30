c6f4ddaf4769171985c3d8de8ba684b76246ae07
najamoe <naja0718@stud.kea.dk>
2023-04-30 15:02:49 +0200

    added attributes in projectModel  and taskModel

--- src/main/java/com/alphaS/alphasolutions/model/ProjectModel.java
+++ src/main/java/com/alphaS/alphasolutions/model/ProjectModel.java
@@ -1,4 +1,60 @@
 package com.alphaS.alphasolutions.model;
 
+import java.time.LocalDate;
+
 public class ProjectModel {
+
+    private String projectName;
+    private String projectDescriptopn;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public ProjectModel(String projectName, String projectDescriptopn, LocalDate startDate, LocalDate endDate) {
+        this.projectName = projectName;
+        this.projectDescriptopn = projectDescriptopn;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDescriptopn() {
+        return projectDescriptopn;
+    }
+
+    public void setProjectDescriptopn(String projectDescriptopn) {
+        this.projectDescriptopn = projectDescriptopn;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectModel{" +
+                "projectName='" + projectName + '\'' +
+                ", projectDescriptopn='" + projectDescriptopn + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
 }
--- src/main/java/com/alphaS/alphasolutions/model/TaskModel.java
+++ src/main/java/com/alphaS/alphasolutions/model/TaskModel.java
@@ -1,4 +1,60 @@
 package com.alphaS.alphasolutions.model;
 
+import java.time.LocalTime;
+
 public class TaskModel {
+
+    private String taskName;
+    private String taskDescription;
+    private LocalTime estTime;
+    private String jobTitleNeeded;
+
+    public TaskModel(String taskName, String taskDescription, LocalTime estTime, String jobTitleNeeded) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.estTime = estTime;
+        this.jobTitleNeeded = jobTitleNeeded;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalTime getEstTime() {
+        return estTime;
+    }
+
+    public void setEstTime(LocalTime estTime) {
+        this.estTime = estTime;
+    }
+
+    public String getJobTitleNeeded() {
+        return jobTitleNeeded;
+    }
+
+    public void setJobTitleNeeded(String jobTitleNeeded) {
+        this.jobTitleNeeded = jobTitleNeeded;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskModel{" +
+                "taskName='" + taskName + '\'' +
+                ", taskDescription='" + taskDescription + '\'' +
+                ", estTime=" + estTime +
+                ", jobTitleNeeded='" + jobTitleNeeded + '\'' +
+                '}';
+    }
 }
