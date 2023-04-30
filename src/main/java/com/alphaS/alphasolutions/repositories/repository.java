e2f923604b091502d39521d624764d400a053706
najamoe <naja0718@stud.kea.dk>
2023-04-30 15:24:15 +0200

    added empty methods in repository

--- src/main/java/com/alphaS/alphasolutions/repositories/repository.java
+++ src/main/java/com/alphaS/alphasolutions/repositories/repository.java
@@ -1,7 +1,50 @@
 package com.alphaS.alphasolutions.repositories;
 
+import com.alphaS.alphasolutions.model.*;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class repository {
+
+    //Method for signin in
+    public UserModel signin(){
+
+    }
+
+    //Method for creating new project
+    public ProjectModel createProject(){
+
+    }
+
+    //Method for creating new tasks
+    public TaskModel createTask(){
+
+    }
+    public ClientModel createClient(){
+
+    }
+    public ClientModel searchClient(){
+
+    }
+
+    public TeamModel createTeam(){
+
+    }
+
+    //Method for searching projects
+    public ProjectModel searchProject(){
+
+    }
+
+    //Method for editing projects
+    public ProjectModel editProject(){
+
+    }
+    //Method for deleting projects
+    public ProjectModel deleteProject(){
+
+    }
+
+
+
 }
