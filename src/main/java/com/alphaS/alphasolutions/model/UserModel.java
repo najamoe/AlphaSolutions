6ee2b3eae2ad63f28f5288a7953e5bab56a6f5f1
najamoe <naja0718@stud.kea.dk>
2023-04-30 14:58:47 +0200

    added attributes in UserModel

--- src/main/java/com/alphaS/alphasolutions/model/UserModel.java
+++ src/main/java/com/alphaS/alphasolutions/model/UserModel.java
@@ -2,5 +2,102 @@ package com.alphaS.alphasolutions.model;
 
 public class UserModel {
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private int password;
+    private int phoneNo;
+    private String country;
+    private String jobTitle;
 
+    public UserModel(String firstName, String lastName, String email, String username, int password, int phoneNo, String country, String jobTitle) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.phoneNo = phoneNo;
+        this.country = country;
+        this.jobTitle = jobTitle;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password=" + password +
+                ", phoneNo=" + phoneNo +
+                ", country='" + country + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
 }
+
