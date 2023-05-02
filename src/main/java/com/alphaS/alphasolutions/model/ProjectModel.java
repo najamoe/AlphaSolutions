35fe5c18020b60dc43c6ef56c34e1a33a4075df8
najamoe <najamoe@outlook.dk>
2023-05-02 16:43:14 +0200

    changed the projectmodel so the variables matches the ones in our sql database

--- src/main/java/com/alphaS/alphasolutions/model/ProjectModel.java
+++ src/main/java/com/alphaS/alphasolutions/model/ProjectModel.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class ProjectModel {
 
+    private int projectId;
     private String projectName;
     private String projectDescriptopn;
     private LocalDate startDate;
@@ -11,11 +12,26 @@ public class ProjectModel {
     private int client_id;
     private int user_id;
 
-    public ProjectModel(String projectName, String projectDescriptopn, LocalDate startDate, LocalDate endDate) {
+    public ProjectModel(int projectId, String projectName, String projectDescriptopn, LocalDate startDate, LocalDate endDate, int client_id, int user_id) {
+        this.projectId = projectId;
         this.projectName = projectName;
         this.projectDescriptopn = projectDescriptopn;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.client_id = client_id;
+        this.user_id = user_id;
+    }
+
+    public ProjectModel() {
+
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
     }
 
     public String getProjectName() {
@@ -50,13 +66,32 @@ public class ProjectModel {
         this.endDate = endDate;
     }
 
+    public int getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(int client_id) {
+        this.client_id = client_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
     @Override
     public String toString() {
         return "ProjectModel{" +
-                "projectName='" + projectName + '\'' +
+                "projectId=" + projectId +
+                ", projectName='" + projectName + '\'' +
                 ", projectDescriptopn='" + projectDescriptopn + '\'' +
                 ", startDate=" + startDate +
                 ", endDate=" + endDate +
+                ", client_id=" + client_id +
+                ", user_id=" + user_id +
                 '}';
     }
 }
