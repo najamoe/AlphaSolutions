355c67ae4a2d6d4f361ccfaacd51eb72cca9bc67
najamoe <naja0718@stud.kea.dk>
2023-05-04 21:40:05 +0200

    employee-controller created

---
+++ src/main/java/com/alphaS/alphasolutions/controllers/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.alphaS.alphasolutions.controllers;
+
+import com.alphaS.alphasolutions.model.EmployeeModel;
+import com.alphaS.alphasolutions.repositories.EmployeeRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLException;
+
+@Controller
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @PostMapping("/signin")
+    public String signinpostmapping(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model){
+        try {
+            EmployeeModel employee = employeeRepository.logIn(username, password);
+            if (employee != null) {
+                session.setAttribute("user", employee); // store user in session
+                return "redirect:/dashboard"; // redirect to dashboard page if login is successful
+            } else {
+                model.addAttribute("error", "Username or password incorrect"); // add error message to model
+                return "signin"; // return sign-in page if login is unsuccessful
+            }
+        } catch (SQLException e) {
+            model.addAttribute("error", "An error occurred during login"); // add error message to model
+            return "signin"; // return sign-in page if an error occurs during login
+        }
+    }
+
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/signin";
+    }
+}
