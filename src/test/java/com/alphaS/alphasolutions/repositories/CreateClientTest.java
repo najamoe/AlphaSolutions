3aa19d097e0e342359170b36b523634fd9bad26c
najamoe <najamoe@outlook.dk>
2023-05-04 16:14:02 +0200

    clientTest in progress

--- src/test/java/com/alphaS/alphasolutions/repositories/CreateClientTest.java
+++ src/test/java/com/alphaS/alphasolutions/repositories/CreateClientTest.java
@@ -1,13 +1,18 @@
 package com.alphaS.alphasolutions.repositories;
 
+import ch.qos.logback.core.net.server.Client;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateClientTest {
 
     @Test
-    void createClient() {
+    void CreateClient() throws SQLException {
+        String result = ProjectRepository.createClient("Test Client", "12345678", "John Doe", "98765432", "Byvej 69", "3400", "DK", "01");
+        assertEquals("Client successfully added to database", result);
     }
 
     @Test
