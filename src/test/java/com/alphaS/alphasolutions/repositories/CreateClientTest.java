0068431940662c1da859410072ed03f29fde5394
najamoe <najamoe@outlook.dk>
2023-05-04 13:01:16 +0200

    created test class for CreateClientTest

---
+++ src/test/java/com/alphaS/alphasolutions/repositories/CreateClientTest.java
@@ -0,0 +1,24 @@
+package com.alphaS.alphasolutions.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateClientTest {
+
+    @Test
+    void createClient() {
+    }
+
+    @Test
+    void editClient() {
+    }
+
+    @Test
+    void searchClients() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+}
