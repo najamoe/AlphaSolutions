0ef931b943fe045704672e63b96399e7838cb7fb
najamoe <naja0718@stud.kea.dk>
2023-05-05 12:24:39 +0200

    refactored TaskCompassController.java to MainController

--- src/main/java/com/alphaS/alphasolutions/controllers/MainController.java
+++ src/main/java/com/alphaS/alphasolutions/controllers/MainController.java
@@ -4,13 +4,8 @@ package com.alphaS.alphasolutions.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
-
 @Controller
-public class TaskCompassController {
+public class MainController {
 
     @GetMapping("")
     public String Index(){
