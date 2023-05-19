a222f197d846f6ff0f1bb35d6196aa3231c607c3
MathildeTrendy <113180048+MathildeTrendy@users.noreply.github.com>
2023-05-19 10:05:58 +0200

    udkommenteret delete team i team controller

--- src/main/java/com/alphaS/alphasolutions/controllers/TeamController.java
+++ src/main/java/com/alphaS/alphasolutions/controllers/TeamController.java
@@ -161,11 +161,13 @@ public class TeamController {
 
     //TODO
 
-    @PostMapping("/project/{projectId}/subproject/delete")
+  /*  @PostMapping("/project/{projectId}/subproject/delete")
     public String deleteSubproject(@RequestParam int teamId, @RequestParam int userId) throws SQLException {
         String message = teamService.deleteEmployeeFromTeam(teamId, userId);
         return message;
     }
+
+*/
 
     //TODO
 
