5964e6f5ecd5b9f9999a2f00be7bfa089d452dbd
najamoe <najamoe@outlook.dk>
2023-05-20 14:48:23 +0200

    added so that a createProject only gets created for the user who is logged in, and can only be read by the employee who is logged in

--- src/main/java/com/alphaS/alphasolutions/service/ClientService.java
+++ src/main/java/com/alphaS/alphasolutions/service/ClientService.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Service
 public class ClientService {
     private final ClientRepository clientRepository;
-
     public ClientService(ClientRepository repository) {
         this.clientRepository = repository;
     }
