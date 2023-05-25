9547eb7e09ec71340dea7e29ec84daca06f9c2e7
najamoe <najamoe@outlook.dk>
2023-05-25 13:29:37 +0200

    updated sql

--- src/mysql/init/2-data.sql
+++ src/mysql/init/2-data.sql
@@ -6,10 +6,11 @@ VALUES
     ('Mike', 'Johnson', 'mikejohnson@example.com', 'mikejohnson', 'password3', 23478901);
 
 -- Insert sample data into the Client table
-INSERT INTO taskcompass.Client (client_name, contact_po_no, contact_person, company_po_no, address, zip_code)
+INSERT INTO taskcompass.Client (client_name, contact_po_no, contact_person, company_po_no, address, zip_code, country)
 VALUES
-    ('ABC Company', 1234789, 'John Smith', 9876521, '123 Main St', 12345),
-    ('XYZ Corporation', 9854321, 'Jane Doe', 1234789, '456 Elm St', 54321);
+    ('ABC Company', 1234789, 'John Smith', 9876521, '123 Main St', 1234, 'DK'),
+    ('XYZ Corporation', 9854321, 'Jane Doe', 1234789, '456 Elm St', 5432, 'DK'),
+    ('Pepsi Corp', 48791326, 'John Hancock', 21467376, 'Byvej 6', 2950, 'DK');
 
 
 -- Insert sample data into the Project table
@@ -17,12 +18,10 @@ INSERT INTO taskcompass.Project (project_name, project_description, start_date, 
 VALUES
     ('Project 1', 'Description for Project 1', '2023-01-01', '2023-02-28', 1, 1),
     ('Project 2', 'Description for Project 2', '2023-03-01', '2023-04-30', 2, 2),
-    ('Project 3', 'Description for Project 3', '2023-03-01', '2023-04-30', 2, 2),
+    ('Project 3', 'Description for Project 3', '2023-03-01', '2023-04-30', 3, 2),
     ('Project 4', 'Description for Project 2', '2023-03-01', '2023-04-30', 1, 1),
-    ('Project 5', 'Description for Project 1', '2023-01-01', '2023-02-28', 1, 1),
-    ('Project 6', 'Description for Project 2', '2023-03-01', '2023-04-30', 2, 2);
-
-
+    ('Project 5', 'Description for Project 1', '2023-01-01', '2023-02-28', 2, 2),
+    ('Project 6', 'Description for Project 2', '2023-03-01', '2023-04-30', 3, 3);
 
 INSERT INTO taskcompass.Subproject (sub_project_name, sub_project_description, project_id)
 VALUES
@@ -39,19 +38,22 @@ VALUES
     ('Subproject 5', 'Description for Subproject 2', 5),
     ('Subproject 6', 'Description for Subproject 3', 6);
 
-INSERT INTO taskcompass.Task (task_name, description_task, est_time, subproject_id)
+INSERT INTO taskcompass.Task (task_name, description_task, est_days, est_hours, est_minutes, subproject_id)
 VALUES
-    ('Task 1', 'Description for Task 1', '10:00:00', '1'),
-    ('Task 2', 'Description for Task 2', '05:30:00', '2'),
-    ('Task 3', 'Description for Task 3', '08:45:00', '3'),
-    ('Task 4', 'Description for Task 1', '10:00:00', '4'),
-    ('Task 5', 'Description for Task 2', '05:30:00', '5'),
-    ('Task 6', 'Description for Task 3', '08:45:00', '6'),
-    ('Task 1', 'Description for Task 1', '10:00:00', '1'),
-    ('Task 2', 'Description for Task 2', '05:30:00', '2'),
-    ('Task 3', 'Description for Task 3', '08:45:00', '3'),
-    ('Task 4', 'Description for Task 1', '10:00:00', '4'),
-    ('Task 5', 'Description for Task 2', '05:30:00', '5'),
-    ('Task 6', 'Description for Task 3', '08:45:00', '6');
+    ('Task 1', 'Description for Task 1', 0, 10, 0, 1),
+    ('Task 2', 'Description for Task 2', 0, 5, 30, 2),
+    ('Task 3', 'Description for Task 3', 0, 8, 45, 3),
+    ('Task 4', 'Description for Task 1', 0, 10, 0, 4),
+    ('Task 5', 'Description for Task 2', 0, 5, 30, 5),
+    ('Task 6', 'Description for Task 3', 0, 8, 45, 6),
+    ('Task 1', 'Description for Task 1', 0, 10, 0, 1),
+    ('Task 2', 'Description for Task 2', 0, 5, 30, 2),
+    ('Task 3', 'Description for Task 3', 0, 8, 45, 3),
+    ('Task 4', 'Description for Task 1', 0, 10, 0, 4),
+    ('Task 5', 'Description for Task 2', 0, 5, 30, 5),
+    ('Task 6', 'Description for Task 3', 0, 8, 45, 6);
 
 
+UPDATE taskcompass.client SET project_id = 1 WHERE client_id = 1;
+UPDATE taskcompass.client SET project_id = 2 WHERE client_id = 2;
+UPDATE taskcompass.client SET project_id = 3 WHERE client_id = 3;
