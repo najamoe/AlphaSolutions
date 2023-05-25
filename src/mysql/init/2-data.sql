d26c04ba4c081c8ba3119422dfd58b94fe5d4baf
najamoe <najamoe@outlook.dk>
2023-05-25 10:28:17 +0200

    inserted more test data

--- src/mysql/init/2-data.sql
+++ src/mysql/init/2-data.sql
@@ -21,9 +21,9 @@ VALUES
     ('Project 4', 'Description for Project 2', '2023-03-01', '2023-04-30', 1, 1),
     ('Project 5', 'Description for Project 1', '2023-01-01', '2023-02-28', 1, 1),
     ('Project 6', 'Description for Project 2', '2023-03-01', '2023-04-30', 2, 2);
--- Insert sample data into the Task table
 
--- Insert sample data into the Sub_project table
+
+
 INSERT INTO taskcompass.Subproject (sub_project_name, sub_project_description, project_id)
 VALUES
     ('Subproject 1', 'Description for Subproject 1', 1),
@@ -31,10 +31,22 @@ VALUES
     ('Subproject 3', 'Description for Subproject 3', 3),
     ('Subproject 4', 'Description for Subproject 1', 4),
     ('Subproject 5', 'Description for Subproject 2', 5),
+    ('Subproject 6', 'Description for Subproject 3', 6),
+    ('Subproject 4', 'Description for Subproject 1', 1),
+    ('Subproject 5', 'Description for Subproject 2', 2),
+    ('Subproject 6', 'Description for Subproject 3', 3),
+    ('Subproject 4', 'Description for Subproject 1', 4),
+    ('Subproject 5', 'Description for Subproject 2', 5),
     ('Subproject 6', 'Description for Subproject 3', 6);
 
 INSERT INTO taskcompass.Task (task_name, description_task, est_time, subproject_id)
 VALUES
+    ('Task 1', 'Description for Task 1', '10:00:00', '1'),
+    ('Task 2', 'Description for Task 2', '05:30:00', '2'),
+    ('Task 3', 'Description for Task 3', '08:45:00', '3'),
+    ('Task 4', 'Description for Task 1', '10:00:00', '4'),
+    ('Task 5', 'Description for Task 2', '05:30:00', '5'),
+    ('Task 6', 'Description for Task 3', '08:45:00', '6'),
     ('Task 1', 'Description for Task 1', '10:00:00', '1'),
     ('Task 2', 'Description for Task 2', '05:30:00', '2'),
     ('Task 3', 'Description for Task 3', '08:45:00', '3'),
